ae741cb29945ba018ec197a3944523cb4a07fdab
lcy <mochyli@163.com>
2020-04-22 16:32:54 +0800

    交易查询 添加loading效果
    Signed-off-by: lcy <mochyli@163.com>

--- src/views/manage_exchange/trade.vue
+++ src/views/manage_exchange/trade.vue
@@ -1,70 +1,69 @@
 <!-- 交易查询 -->
 <template>
-    <Row>
-        <Col span="24">
-            <Row>
-                <Card style="margin-top:10px;">
-                    <p slot="title">
-                        {{$t('nav.jycx')}}
-                        <Button type="primary" @click="download()">{{$t('systemlog.dc')}}</Button>
-                    </p>
-                    <p style="margin-bottom: 20px" class="input_p">
-                        <span>
-                            {{$t('exchange.jysc')}}：
-                            <Select v-model="formData.market" style="width: 140px">
-                                <Option :value="0">{{$t('common.qb')}}</Option>
-                                <Option v-for="item in marketList" :value="item.market" :key="item.market">{{ item.market }}</Option>
-                            </Select>
-                        </span>
-                        <span>
-                            {{$t('common.yhm')}}：
-                            <Input v-model.trim="formData.username" clearable style="width: 140px"
-                                   :placeholder="$t('common.qsryhm')"></Input>
-                        </span>
-                        <span>
-                            {{$t('exchange.yhwtid')}}：
-                            <Input v-model="formData.orderBookId" clearable style="width: 140px"
-                                   :placeholder="$t('exchange.yhwtid')"></Input>
-                        </span>
-                        <span>
-                            {{$t('exchange.cjsj')}}：
-                            <DatePicker type="datetime" v-model="formData.startTime" :placeholder="$t('common.kssj')"
-                                        format="yyyy-MM-dd HH:mm:ss"
-                                        style="width: 160px"></DatePicker>
-                            <DatePicker type="datetime" v-model="formData.endTime" :placeholder="$t('common.jssj')"
-                                        format="yyyy-MM-dd HH:mm:ss"
-                                        style="width: 160px"></DatePicker>
-                        </span>
-                        <span>
-                            {{$t('exchange.cjjg')}}：
-                            <Select v-model="formData.preDealPrice" style="width:60px">
-                                <Option :value="0">{{$t('common.qb')}}</Option>
-                                <Option :value="1">{{$t('exchange.xy')}}</Option>
-                                <Option :value="2">{{$t('exchange.dy')}}</Option>
-                                <Option :value="3">{{$t('exchange.dengy')}}</Option>
-                            </Select>
-                            <numberbox :min="0" v-model="formData.dealPrice" style="width: 100px;border:1px solid #dddee1"></numberbox>
-                        </span>
-                        <span>
-                            {{$t('exchange.cjl')}}：
-                            <Select v-model="formData.preDealAmount" style="width: 60px">
-                                <Option :value="0">{{$t('common.qb')}}</Option>
-                                <Option :value="1">{{$t('exchange.xy')}}</Option>
-                                <Option :value="2">{{$t('exchange.dy')}}</Option>
-                                <Option :value="3">{{$t('exchange.dengy')}}</Option>
-                            </Select>
-                            <numberbox v-model="formData.dealAmount" style="width: 100px;border:1px solid #dddee1"></numberbox>
-                        </span>
-                        <span> <Checkbox v-model="formData.excludeSpecialUser">{{$t('exchange.pctszh')}}</Checkbox></span>
-                        <Button type="primary" @click="curPage=1;getAuditing()">{{$t('common.cx')}}</Button>
-                    </p>
-                    <Table ref="table" :columns="columns" :data="datas"></Table>
-                    <Page :current="curPage" :total="total" @on-change="changePage" :page-size="size"
-                          style="text-align:center;margin-top:20px;"></Page>
-                </Card>
-            </Row>
-        </Col>
-    </Row>
+    <Card class="spax">
+        <div class="spaxWp" v-if="spax">
+            <div class="loader">
+                <svg viewBox="25 25 50 50" class="circular"><circle cx="50" cy="50" r="20" fill="none" stroke-width="5" stroke-miterlimit="10" class="path"></circle></svg>
+            </div>
+        </div>
+        <p slot="title">
+            {{$t('nav.jycx')}}
+            <Button type="primary" @click="download()">{{$t('systemlog.dc')}}</Button>
+        </p>
+        <p style="margin-bottom: 20px" class="input_p">
+            <span>
+                {{$t('exchange.jysc')}}：
+                <Select v-model="formData.market" style="width: 140px">
+                    <Option :value="0">{{$t('common.qb')}}</Option>
+                    <Option v-for="item in marketList" :value="item.market" :key="item.market">{{ item.market }}</Option>
+                </Select>
+            </span>
+            <span>
+                {{$t('common.yhm')}}：
+                <Input v-model.trim="formData.username" clearable style="width: 140px"
+                        :placeholder="$t('common.qsryhm')"></Input>
+            </span>
+            <span>
+                {{$t('exchange.yhwtid')}}：
+                <Input v-model="formData.orderBookId" clearable style="width: 140px"
+                        :placeholder="$t('exchange.yhwtid')"></Input>
+            </span>
+            <span>
+                {{$t('exchange.cjsj')}}：
+                <DatePicker type="datetime" v-model="formData.startTime" :placeholder="$t('common.kssj')"
+                            format="yyyy-MM-dd HH:mm:ss"
+                            style="width: 160px"></DatePicker>
+                <DatePicker type="datetime" v-model="formData.endTime" :placeholder="$t('common.jssj')"
+                            format="yyyy-MM-dd HH:mm:ss"
+                            style="width: 160px"></DatePicker>
+            </span>
+            <span>
+                {{$t('exchange.cjjg')}}：
+                <Select v-model="formData.preDealPrice" style="width:60px">
+                    <Option :value="0">{{$t('common.qb')}}</Option>
+                    <Option :value="1">{{$t('exchange.xy')}}</Option>
+                    <Option :value="2">{{$t('exchange.dy')}}</Option>
+                    <Option :value="3">{{$t('exchange.dengy')}}</Option>
+                </Select>
+                <numberbox :min="0" v-model="formData.dealPrice" style="width: 100px;border:1px solid #dddee1"></numberbox>
+            </span>
+            <span>
+                {{$t('exchange.cjl')}}：
+                <Select v-model="formData.preDealAmount" style="width: 60px">
+                    <Option :value="0">{{$t('common.qb')}}</Option>
+                    <Option :value="1">{{$t('exchange.xy')}}</Option>
+                    <Option :value="2">{{$t('exchange.dy')}}</Option>
+                    <Option :value="3">{{$t('exchange.dengy')}}</Option>
+                </Select>
+                <numberbox v-model="formData.dealAmount" style="width: 100px;border:1px solid #dddee1"></numberbox>
+            </span>
+            <span> <Checkbox v-model="formData.excludeSpecialUser">{{$t('exchange.pctszh')}}</Checkbox></span>
+            <Button type="primary" @click="curPage=1;getAuditing()">{{$t('common.cx')}}</Button>
+        </p>
+        <Table ref="table" :columns="columns" :data="datas"></Table>
+        <Page :current="curPage" :total="total" @on-change="changePage" :page-size="size"
+                style="text-align:center;margin-top:20px;"></Page>
+    </Card>
 </template>
 
 <script>
@@ -76,6 +75,7 @@
         components: {Numberbox},
         data () {
             return {
+                spax: false,
                 curPage: 1,
                 total: 0,
                 size: 10,
@@ -139,6 +139,7 @@
                 }
             },
             getAuditing () {
+                this.spax = true;
                 let D = JSON.stringify(this.formData);
                 let data = JSON.parse(D);
                 data.startTime = data.startTime ? util.dateToStr(new Date(data.startTime)) : null;
@@ -154,6 +155,7 @@
                     (res, total) => {
                         this.total = total;
                         this.datas = res;
+                        this.spax = false;
                     });
             },
             changePage (page) {
@@ -201,7 +203,8 @@
                         data: dataExp
                     });
                 });
-            }
+            },
+            
         }
     };
 </script>
@@ -228,5 +231,59 @@
             display: inline-block;
             margin: 10px 10px 0 0;
         }
+    }
+
+    .spaxWp{
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        z-index: 1000;
+        transform: translate(-50%,-50%);
+        padding: 40px;
+        box-shadow: 0 0 20px rgba(0,0,0,.1);
+        background-color: #fff;
+        border-radius: 10px;
+    }
+    .loader {
+        width: 30px;
+        height: 30px;
+        position: relative;
+        margin: 0 auto;
+    }
+    .circular {
+        -webkit-animation: rotate 2s linear infinite;
+        animation: rotate 2s linear infinite;
+        height: 100%;
+        -webkit-transform-origin: center center;
+        transform-origin: center center;
+        width: 100%;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+    }
+    @keyframes rotate{
+        to{-webkit-transform:rotate(1turn);
+        transform:rotate(1turn)}
+    }
+    .path {
+        stroke-dasharray: 1,200;
+        stroke-dashoffset: 0;
+        -webkit-animation: dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;
+        animation: dash 1.5s ease-in-out infinite,color 6s ease-in-out infinite;
+        stroke-linecap: round;
+    }
+    @keyframes dash{
+        0%{stroke-dasharray:1,200;stroke-dashoffset:0}
+        50%{stroke-dasharray:89,200;stroke-dashoffset:-35}
+        to{stroke-dasharray:89,200;stroke-dashoffset:-124}
+    }
+    @keyframes color{
+        0%,to{stroke:#d62d20}
+        40%{stroke:#0057e7}
+        66%{stroke:#008744}
+        80%,90%{stroke:#ffa700}
     }
 </style>
