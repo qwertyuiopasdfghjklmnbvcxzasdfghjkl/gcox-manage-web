dfb01551dbe5638637b457c8cf8a7f5822887085
mochyli <mochyli@163.com>
2020-09-10 12:00:01 +0800

    提币审核国际化
    Signed-off-by: mochyli <mochyli@163.com>

--- src/router.js
+++ src/router.js
@@ -814,7 +814,7 @@ export const financeRouter = [
     {
         path: '/finance_withdraw_review',
         name: 'finance_withdraw_review',
-        title: 'nav.tbsh1',
+        title: 'finance.tbsh1',
         component: Main,
         icon: 'soup-can-outline',
         meta: {
@@ -822,7 +822,7 @@ export const financeRouter = [
         },
         children: [
             {
-                path: 'index', title: 'nav.tbsh1', name: 'finance_withdraw_review_index', component: resolve => {
+                path: 'index', title: 'finance.tbsh1', name: 'finance_withdraw_review_index', component: resolve => {
                     require(['./views/manage_finance/withdraw_review.vue'], resolve);
                 }
             }
